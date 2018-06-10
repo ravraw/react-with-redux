dca122588dcf83872f4e3d464d1617fac101ea7d
ravindra rawat <ravinrawat.wd@gmail.com>
2018-06-09 18:58:23 -0600

    added utility function to reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   counter: 0
@@ -7,28 +8,40 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + 1
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter + 1
+    // };
     // break;
     case actionTypes.DECREMENT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - 1
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter - 1
+    // };
     // break;
     case actionTypes.ADD:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + action.payload.value
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter + action.payload.value
+    // };
     // break;
     case actionTypes.SUBSTRACT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - action.payload.value
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter - action.payload.value
+    // };
     //break;
     default:
       return state;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: []
@@ -7,18 +8,23 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result })
-      };
+      });
+    // return {
+    //   ...state,
+    //   results: state.results.concat({ id: new Date(), value: action.result })
+    // };
     case actionTypes.DELETE_RESULT:
       const updatedResults = state.results.filter(
         result => result.id !== action.resultElId
       );
-      return {
-        ...state,
-        results: updatedResults
-      };
+      return updateObject(state, { results: updatedResults });
+
+    // return {
+    //   ...state,
+    //   results: updatedResults
+    // };
     default:
       return state;
   }
